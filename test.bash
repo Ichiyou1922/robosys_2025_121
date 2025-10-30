56e2174296ada0da9cb791b16598db50c5520a0a
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-10-30 13:04:57 +0900

    Add test cases

--- test.bash
+++ test.bash
@@ -9,9 +9,22 @@ ng () {
 
 res=0
 
+### Normal Input###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
-exit $res
+
+###Strange Input###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit ${res}
+
 
